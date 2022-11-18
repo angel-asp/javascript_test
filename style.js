768626e54a6e2ddf18fa64a9bd1ae868e20539a0
angel Sanchez-Panadero <angelasp2001@gmail.com>
2022-11-17 18:12:24 -0600

    added a bunch of javascript

--- style.js
+++ style.js
@@ -14,25 +14,121 @@ THEN I can save my initials and my score
 
 var startButton = document.querySelector("#startButton");
 var questions = document.querySelector(".questions");
-var answers = document.querySelector(".extratext");
+var extratext = document.querySelector(".extratext");
 var timer = document.querySelector(".timer");
-
+var multipleChoiceButton = document.querySelector("#multipleChoiceButton");
 var secondsLeft = 90;
+var hideStartButtion = document.querySelector("#hideLi");
+//var promtOne = document.querySelector("#promtOne");
+//var promtTwo = document.querySelector("#promtTwo");
+//var promtThree = document.querySelector("#promtThree");
+//var promtFour = document.querySelector("#promtFour");
+//future box where you type in your name
+var nameBox = "1";
 
 
+//vars used for question 1
+ var questionOne = "What color is the sky?";
+
+ var answerOneOne = document.createElement("li")
+ answerOneOne.textContent = "blue";
+ answerOneOne.className = "multipleChoiceButton";
+ answerOneOne.addEventListener("click", setQuestionTwo);
+ answerOneOne.setAttribute("id", "promtOne");
+
+ var answerOneTwo =  document.createElement("li");
+ answerOneTwo.textContent = "red";
+ answerOneTwo.className = "multipleChoiceButton";
+ answerOneTwo.addEventListener("click", wrongAnswer);
+ answerOneTwo.setAttribute("id", "promtTwo");
+
+ var answerOneThree =  document.createElement("li");
+ answerOneThree.textContent = "green";
+ answerOneThree.className = "multipleChoiceButton";
+ answerOneThree.addEventListener("click", wrongAnswer);
+ answerOneThree.setAttribute("id", "promtThree");
+
+ var answerOneFour = document.createElement("li");
+ answerOneFour.textContent = "purple";
+ answerOneFour.className = "multipleChoiceButton";
+ answerOneFour.addEventListener("click", wrongAnswer);
+ answerOneFour.setAttribute("id", "promtFour");
+
+//first question being set
+function setAnswers () {
+    questions.textContent = questionOne;
+    extratext.textContent = "";
+    multipleChoiceButton.appendChild(answerOneOne);
+    multipleChoiceButton.appendChild(answerOneTwo);
+    multipleChoiceButton.appendChild(answerOneThree);
+    multipleChoiceButton.appendChild(answerOneFour);
+} 
+
+//function when you press wrong buttion
+function wrongAnswer() {
+    //set a fuction to subtract 15 sec from countdown
+    setQuestionTwo();
+}
+//sets up second question and answers
+function setQuestionTwo() {
+    questions.textContent = "what animal can fly";
+    answerOneOne.textContent = answerTwoOne;
+    answerOneTwo.textContent = answerTwoTwo;
+    answerOneThree.textContent = answerTwoThree;
+    answerOneFour.textContent = answerTwoFour;
+}
+
+var questionTwo = "What animal can fly";
+
+var answerTwoOne = "pig";
+var answerTwoTwo = "cat";
+var answerTwoThree = "bird";
+var answerTwoFour = "pig";
+
+
+
+
+
+
+
+
+
+
+
+
+//function that starts quiz
+function quiz() {
+   hideStartButtion.textContent = "";
+    startButton.className = "orderedlistBox";
+    startButton.setAttribute("id", "nothingButtion")
+    setAnswers();
+}
+
+// function that loads at end of test
+function loadEndPage() {
+    questions.textContent = "All done!";
+    extratext.textContent = "your final score is " + secondsLeft;
+    startButton.textContent = "Enter Name " + nameBox;
+}
+
+
+// start quiz 
 
 
 startButton.addEventListener("click", function() {
-    
+        
         var timeInterval = setInterval(function() {
         secondsLeft--;
         timer.textContent = " timer " + secondsLeft ;
+        
     
         if(secondsLeft === 0) {
-            clearInterval()
+            clearInterval(timeInterval);
             console.log("yayyy");
-        }it 
+            timer.textContent = "timer";
+            loadEndPage()
+        } 
     }, 1000);
-    questions.textContent = "Question number jaun";
+    quiz()
 })
 
